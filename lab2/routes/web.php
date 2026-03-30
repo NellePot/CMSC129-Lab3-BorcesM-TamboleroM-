6b96345c7ebfb3d47cbecdc9371eeefea73f866d
Michaela F. Borces <mfborces@up.edu.ph>
2026-03-30 11:38:30 +0700

    handled routes

--- lab2/routes/web.php
+++ lab2/routes/web.php
@@ -5,10 +5,10 @@ Route::get('/', function () {
     return redirect()->route('inventory.index');
 });
 
-Route::resource('inventory', InventoryController::class);
-
 // Trash routes
 Route::get('inventory/trashed', [InventoryController::class, 'trashed'])->name('inventory.trashed');
 Route::patch('inventory/{id}/restore', [InventoryController::class, 'restore'])->name('inventory.restore');
 Route::delete('inventory/{id}/force-delete', [InventoryController::class, 'forceDelete'])->name('inventory.forceDelete');
 Route::get('inventory/critical', [InventoryController::class, 'critical'])->name('inventory.critical');
+
+Route::resource('inventory', InventoryController::class);
