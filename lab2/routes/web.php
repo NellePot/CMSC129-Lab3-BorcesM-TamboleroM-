5a04c24af61ab972fb1662a91a453032471721d8
Michaela F. Borces <mfborces@up.edu.ph>
2026-04-26 23:19:19 +0700

    added api route

--- lab2/routes/web.php
+++ lab2/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 use App\Http\Controllers\InventoryController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatBotController;
 
 Route::get('/', function () {
     return redirect()->route('inventory.index');
@@ -11,6 +13,6 @@ Route::patch('inventory/{id}/restore', [InventoryController::class, 'restore'])-
 Route::delete('inventory/{id}/force-delete', [InventoryController::class, 'forceDelete'])->name('inventory.forceDelete');
 Route::get('inventory/critical', [InventoryController::class, 'critical'])->name('inventory.critical');
 
-Route::post('/chatbot', [ChatBotController::class, 'sendMessage'])->name('chat');
-
 Route::resource('inventory', InventoryController::class);
+
+Route::post('/chatbot', [ChatBotController::class, 'sendMessage'])->name('chat');
